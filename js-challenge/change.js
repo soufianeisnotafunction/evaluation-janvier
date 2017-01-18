9cd0b56cde8f1beb3f4b02d5c0d95177efed1ffa
Soufiane Boutallaka <s.boutallaka@gmail.com>
2017-01-18 17:27:15 +0100

    add : change.js

--- js-challenge/change.js
+++ js-challenge/change.js
@@ -1,6 +1,14 @@
-let mot = ['bateau', 'avion', 'voiture'];
+var vehicules = [ "bateau", "avion", "voiture" ];
+var tab = [];
 
-let obj = {};
+vehicules.map(word => tab.push({ title: word, img: word + ".jpeg" }));
+console.log(tab);
 
-mot = mot.map(function(a){return a +'.jpeg'});
-console.log(mot)
+vehicules = vehicules.map(a => a + ".jpeg");
+console.log(vehicules);
+
+var aDesRoues = vehicules.slice(1);
+console.log(aDesRoues);
+
+var screamName = aDesRoues.map(a => a.substring(0, a.length - 5).toUpperCase());
+console.log(screamName);
