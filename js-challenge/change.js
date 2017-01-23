30ae2b1e80496304a374f22595856d6394cc196b
Soufiane Boutallaka <s.boutallaka@gmail.com>
2017-01-23 15:28:37 +0100

    add: css-challence & js-challenge

--- js-challenge/change.js
+++ js-challenge/change.js
@@ -1,7 +1,10 @@
-var vehicules = [ "bateau", "avion", "voiture" ];
+var vehicules = ["bateau", "avion", "voiture"];
 var tab = [];
 
-vehicules.map(word => tab.push({ title: word, img: word + ".jpeg" }));
+vehicules.map(word => tab.push({
+    title: word,
+    img: word + ".jpeg"
+}));
 console.log(tab);
 
 vehicules = vehicules.map(a => a + ".jpeg");
@@ -11,4 +14,4 @@ var aDesRoues = vehicules.slice(1);
 console.log(aDesRoues);
 
 var screamName = aDesRoues.map(a => a.substring(0, a.length - 5).toUpperCase());
-console.log(screamName);
+console.log(screamName); 
